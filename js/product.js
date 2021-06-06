1cef3255bf5047e97bd27605f1e222c2c715cc6e
carolynegreen <carolynemarie94@gmail.com>
2021-06-06 20:36:12 +0200

    product page fixed

--- js/product.js
+++ js/product.js
@@ -1,7 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 
-fetch("https://kea2sem-4cc6.restdb.io/rest/ideelkaffe" + id, {
+fetch("https://kea2sem-4cc6.restdb.io/rest/ideelkaffe/" + id, {
   method: "GET",
   headers: {
     "x-apikey": "60b8c65d318a330b62f58a37",
